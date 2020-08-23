039b7bf0b13c3c2363b4da13da3b598dcb9f9212
jinho <jinho021712@gmail.com>
2020-08-23 16:13:53 +0900

    레이아웃 수정
    메뉴, 서브메뉴 추가
    * 서브메뉴 추가 시 key값 달라야 함

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,8 @@ function App() {
         </Header>
         <Layout>
           <Sider>
+         
+            
             <Menu
               defaultSelectedKeys={['Dashboard']}
               mode="inline"
@@ -32,6 +34,10 @@ function App() {
                   </span>
                 }
               >
+                <div>
+                  {/* 서브메뉴 추가 시 key값 달라야 함 */}
+                </div>
+
                 <SubMenu
                 title={
                   <span>
@@ -41,8 +47,8 @@ function App() {
                 }
               >
                 <Menu.ItemGroup key='AboutUS' title='서브메뉴 테스트'>
-                  <Menu.Item key='location1'> Programmer_Backend</Menu.Item>
-                  <Menu.Item key='location2'> Programmer_Frontend</Menu.Item>
+                  <Menu.Item key='TEAM1_1'> Programmer_Backend</Menu.Item>
+                  <Menu.Item key='TEAM1_2'> Programmer_Frontend</Menu.Item>
                 </Menu.ItemGroup>
               </SubMenu>
 
@@ -50,28 +56,13 @@ function App() {
                 title={
                   <span>
                     <Icon type="mail" />
-                    <span>TEAM 1</span>
+                    <span>TEAM 2</span>
                   </span>
                 }
               >
                 <Menu.ItemGroup key='AboutUS' title='서브메뉴 테스트'>
-                  <Menu.Item key='location1'> Programmer_Backend</Menu.Item>
-                  <Menu.Item key='location2'> Programmer_Frontend</Menu.Item>
-                </Menu.ItemGroup>
-              </SubMenu>
-
-
-              <SubMenu
-                title={
-                  <span>
-                    <Icon type="mail" />
-                    <span>TEAM 1</span>
-                  </span>
-                }
-              >
-                <Menu.ItemGroup key='AboutUS' title='서브메뉴 테스트'>
-                  <Menu.Item key='location1'> Programmer_Backend</Menu.Item>
-                  <Menu.Item key='location2'> Programmer_Frontend</Menu.Item>
+                  <Menu.Item key='TEAM2_1'> Programmer_Backend</Menu.Item>
+                  <Menu.Item key='TEAM2_2'> Programmer_Frontend</Menu.Item>
                 </Menu.ItemGroup>
               </SubMenu>
 
@@ -80,30 +71,16 @@ function App() {
                 title={
                   <span>
                     <Icon type="mail" />
-                    <span>TEAM 1</span>
+                    <span>TEAM 3</span>
                   </span>
                 }
               >
                 <Menu.ItemGroup key='AboutUS' title='서브메뉴 테스트'>
-                  <Menu.Item key='location1'> Programmer_Backend</Menu.Item>
-                  <Menu.Item key='location2'> Programmer_Frontend</Menu.Item>
+                  <Menu.Item key='TEAM3_1'> Programmer_Backend</Menu.Item>
+                  <Menu.Item key='TEAM3_2'> Programmer_Frontend</Menu.Item>
                 </Menu.ItemGroup>
               </SubMenu>
 
-
-              <SubMenu
-                title={
-                  <span>
-                    <Icon type="mail" />
-                    <span>TEAM 1</span>
-                  </span>
-                }
-              >
-                <Menu.ItemGroup key='AboutUS' title='서브메뉴 테스트'>
-                  <Menu.Item key='location1'> Programmer_Backend</Menu.Item>
-                  <Menu.Item key='location2'> Programmer_Frontend</Menu.Item>
-                </Menu.ItemGroup>
-              </SubMenu>
 
 
               </SubMenu>
@@ -112,11 +89,11 @@ function App() {
           <Layout>
             <Content style={{ padding: '0 50px' }}>
               <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                <Breadcrumb.Item>본문 부분</Breadcrumb.Item>
               </Breadcrumb>
 
             </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design Layout example Created by Shrideep</Footer>
+            <Footer style={{ textAlign: 'center' }}>© 2020 Cotion, Team</Footer>
           </Layout>
         </Layout>
       </Layout>
